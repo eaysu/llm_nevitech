0f7d760d729d3ca2d3182b190c0a349a2c0534d4
eaysu <e.aysu2019@gtu.edu.tr>
2024-07-16 11:39:07 +0300

    prompt structure fixed again

--- llm_comparison/query_data.py
+++ llm_comparison/query_data.py
@@ -1,6 +1,5 @@
 import argparse
 import time
-import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate
@@ -12,8 +11,6 @@ from get_embedding_function import get_embedding_function
 ollama_language_models = ["mistral", "llama3", "gemma2", "qwen2:7b", "llama3:27b", "gemma2:70b", "qwen2:72b"]
 
 CHROMA_PATH = "chroma"
-
-### TEMPLATES FOR OLLAMA MODELS ###
 
 PROMPT_TEMPLATE_EN = """
 Answer the question based only on the following context: {context}
@@ -30,7 +27,7 @@ Yukarıdaki bağlama dayanarak soruyu cevaplayın: {question}
 PROMPT_TEMPLATE_FR = """
 Répondez à la question en vous basant uniquement sur le contexte suivant: {context}
 ---
-Répondez à la question en vous basant sur le contexte ci-dessus : {question}
+Répondez à la question en vous basant sur le contexte ci-dessus: {question}
 """
 
 PROMPT_TEMPLATE_ES = """
@@ -94,7 +91,7 @@ def query_rag(query_text: str, language_model: str):
         response_text = model.invoke(prompt)
     else:
         tokenizer = AutoTokenizer.from_pretrained(language_model)
-        model = AutoModelForCausalLM.from_pretrained(language_model, torch_dtype=torch.bfloat16, device_map="auto", load_in_8bit=True)
+        model = AutoModelForCausalLM.from_pretrained(language_model)
 
         lang_for_tr_llm = detect(query_text)
 
@@ -112,12 +109,11 @@ def query_rag(query_text: str, language_model: str):
             messages = [
                 {"role": "system", "content": "You are a helpful chatbot who always responds friendly."},
                 {"role": "user", "content": prompt}
-            ]    
+            ]
 
-        input_ids = tokenizer.apply_chat_template(messages, return_tensors="pt").to("cuda")
-        outputs = model.generate(input_ids, max_new_tokens=1024, do_sample=True, temperature=0.7, top_p=0.7, top_k=500)
-        response = outputs[0][input_ids.shape[-1]:]
-        response_text = tokenizer.decode(response, skip_special_tokens=True)
+        inputs = tokenizer(messages, return_tensors="pt")
+        outputs = model.generate(**inputs, max_length=512, max_new_tokens=50)
+        response_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
     end_time = time.time()
     elapsed_time = end_time - start_time
@@ -128,6 +124,5 @@ def query_rag(query_text: str, language_model: str):
     formatted_response = f"Seçilen Model: {language_model}\nYanıt: {response_text}\nKaynaklar:\n{formatted_sources}\nSüre: {elapsed_time:.2f} saniye\n"
     print(formatted_response)
     return response_text
-
 
 
