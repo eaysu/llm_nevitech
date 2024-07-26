84d8ff54354381502fbc4d440a81ec5367c5aefb
eaysu <e.aysu2019@gtu.edu.tr>
2024-07-26 15:00:22 +0300

    gpu parameter added

--- llm_comparison/query_data.py
+++ llm_comparison/query_data.py
@@ -165,7 +165,7 @@ def query_rag(query_text: str, language_model: str):
             tokenizer = AutoTokenizer.from_pretrained(language_model)
             
             # Initialize vLLM with the specified model and accelerator (all available GPUs)
-            llm = LLM(model=language_model, dtype="float16")
+            llm = LLM(model=language_model, dtype="float16", tensor_parallel_size=4)
 
             # Define context and query
             context_text = "Masamın üstünde bir suluk, bir bilgisayar ve iki kalem var."
